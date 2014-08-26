7e14c368a1bf9e23918902dc67b1bb1e124d6873
shonna <shonna@interfaceschool.com>
2014-08-26 02:03:34 +0000

    heroku updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :microposts
-
   resources :users
 
+  match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
