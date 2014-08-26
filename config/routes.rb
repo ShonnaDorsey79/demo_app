06126d82d09170d59924983fee7f40f810abdcc5
shonna <shonna@interfaceschool.com>
2014-08-26 05:14:00 +0000

    add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :microposts
   resources :users
-
-  match '/help', to: 'static_pages#help', via: 'get'
+  get "static_pages/home"
+  get "static_pages/help"
+  #match '/help', to: 'static_pages#help', via: 'get'
   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
